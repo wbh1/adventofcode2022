57aa9049303436a6904367a48fe6f4a833d6f279
Will Hegedus <whegedus@linode.com>
2022-12-10 13:28:09 -0500

    day9 part 2 messy code. Got stuck. Thanks Reddit.

--- solutions/day9.go
+++ solutions/day9.go
@@ -18,26 +18,37 @@ type GridPoint struct {
 }
 
 type HeadPoint struct {
+	Parent            *HeadPoint
 	loc               *GridPoint
-	tail              *GridPoint
+	tails             []*HeadPoint
 	TailPointsVisited util.Set
 }
 
 func distance(p1 *GridPoint, p2 *GridPoint) (x, y int) {
-	x = int(math.Abs(float64(p1.x) - float64(p2.x)))
-	y = int(math.Abs(float64(p1.y) - float64(p2.y)))
+	x = p1.x - p2.x
+	y = p1.y - p2.y
 	return
 }
 
 func adjacent(p1 *GridPoint, p2 *GridPoint) bool {
 	x, y := distance(p1, p2)
 
-	return x <= 1 && y <= 1
+	return math.Abs(float64(x)) <= 1 && math.Abs(float64(y)) <= 1
+}
+
+func offset(n int) int {
+	switch {
+	case n == 0:
+		return 0
+	case n > 0:
+		return 1
+	default:
+		return -1
+	}
 }
 
 func (hp *HeadPoint) Move(direction string, count int) {
 	for i := 0; i < count; i++ {
-		headStart := *hp.loc
 		switch direction {
 		case "U":
 			hp.loc.y += 1
@@ -48,12 +59,17 @@ func (hp *HeadPoint) Move(direction string, count int) {
 		case "R":
 			hp.loc.x += 1
 		}
-		// If the head is no longer adjacent to the tail,
-		// the tail moves to where the head just was.
-		if !adjacent(hp.loc, hp.tail) {
-			hp.tail.x = headStart.x
-			hp.tail.y = headStart.y
-			hp.TailPointsVisited.Add(fmt.Sprintf("(%d,%d)", hp.tail.x, hp.tail.y))
+
+		for _, t := range hp.tails {
+			if !adjacent(t.Parent.loc, t.loc) {
+				x, y := distance(t.Parent.loc, t.loc)
+				t.loc.x += offset(x)
+				t.loc.y += offset(y)
+				t.Parent.TailPointsVisited.Add(fmt.Sprintf("(%d,%d)", t.loc.x, t.loc.y))
+
+			} else {
+				break
+			}
 		}
 	}
 }
@@ -62,12 +78,18 @@ func (d *DayNine) SetInput(input []string) {
 	d.input = input
 }
 
-func (d *DayNine) PartOne() string {
-	head := HeadPoint{
+func NewHeadPoint() *HeadPoint {
+	return &HeadPoint{
 		loc:               &GridPoint{},
-		tail:              &GridPoint{},
+		tails:             make([]*HeadPoint, 0),
 		TailPointsVisited: util.Set{"(0,0)"},
 	}
+}
+
+func (d *DayNine) PartOne() string {
+	head := NewHeadPoint()
+	head.tails = append(head.tails, NewHeadPoint())
+	head.tails[0].Parent = head
 
 	for _, line := range d.input {
 		inst := strings.Split(line, " ")
@@ -79,5 +101,25 @@ func (d *DayNine) PartOne() string {
 }
 
 func (d *DayNine) PartTwo() string {
-	return "Not implemented."
+	head := NewHeadPoint()
+	head.tails = make([]*HeadPoint, 9)
+	for i := range head.tails {
+		head.tails[i] = NewHeadPoint()
+		if i < len(head.tails)-1 {
+			head.tails[i].tails = head.tails[i+1:]
+		}
+		if i > 0 {
+			head.tails[i].Parent = head.tails[i-1]
+		} else {
+			head.tails[i].Parent = head
+		}
+	}
+	for _, line := range d.input {
+		inst := strings.Split(line, " ")
+		dir := inst[0]
+		count := util.MustStringToInt(inst[1])
+		head.Move(dir, count)
+	}
+
+	return fmt.Sprintf("%d", len(head.tails[7].TailPointsVisited))
 }
